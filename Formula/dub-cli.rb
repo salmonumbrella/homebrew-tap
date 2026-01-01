1c5892f1c5c36e41ba133f88085d3c8bcf41980e
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2025-12-31 22:11:26 -0800

    dub-cli: update to v0.1.3

--- Formula/dub-cli.rb
+++ Formula/dub-cli.rb
@@ -1,28 +1,28 @@
 class DubCli < Formula
   desc "CLI for Dub.co to manage links, domains, analytics, and workspaces"
   homepage "https://github.com/salmonumbrella/dub-cli"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_darwin_arm64.tar.gz"
-      sha256 "0f965e24f2b3a79e480d4f19c454acd7f32425e21429166deb7aefa37a09c823"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.3/dub-cli_0.1.3_darwin_arm64.tar.gz"
+      sha256 "93e2270ec0b4babe2276363a4b4c071aa387a2b5ea3f249556de27a7d44207b3"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_darwin_amd64.tar.gz"
-      sha256 "db47bce61d7fe2f11a999c0ab2b8ed0c40514bedaa90da9482f6f0888a01eecd"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.3/dub-cli_0.1.3_darwin_amd64.tar.gz"
+      sha256 "57b20f320206f4c9a2511621338b0d7fc4494bff63bc6e39909fafacb6a5ed47"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_linux_arm64.tar.gz"
-      sha256 "9ca1ffec89cfa265d2249cf9a31049fe4d59686e5e66a250ba3f4c37b520d66a"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.3/dub-cli_0.1.3_linux_arm64.tar.gz"
+      sha256 "01353853b174b5b71c4fcc012c985570696e22f8da4f688ff55c0513b6ba6879"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_linux_amd64.tar.gz"
-      sha256 "95d1acf5e381135b1b9fac63ca5a0e054a1a7b92fdbcb824564fad53e6a65580"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.3/dub-cli_0.1.3_linux_amd64.tar.gz"
+      sha256 "52f43cb8dae4c017843b49c43430c54b3d937f8d2558425ffe206fe958c41bb6"
     end
   end
 
