aa73453a435a682af01ed552dc74fa77bb1beee9
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2025-12-31 21:39:36 -0800

    chore: update dub-cli to v0.1.2

--- Formula/dub-cli.rb
+++ Formula/dub-cli.rb
@@ -1,28 +1,28 @@
 class DubCli < Formula
   desc "CLI for Dub.co to manage links, domains, analytics, and workspaces"
   homepage "https://github.com/salmonumbrella/dub-cli"
-  version "0.1.0"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.0/dub-cli_0.1.0_darwin_arm64.tar.gz"
-      sha256 "64d0a21a7479516d6463094d0e50056a67e51fc29b538393eb8656308a68a948"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_darwin_arm64.tar.gz"
+      sha256 "0f965e24f2b3a79e480d4f19c454acd7f32425e21429166deb7aefa37a09c823"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.0/dub-cli_0.1.0_darwin_amd64.tar.gz"
-      sha256 "d5c960d661910083e3e7a734e0a50786f150b54e44bb12975388ccdcee06ef46"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_darwin_amd64.tar.gz"
+      sha256 "db47bce61d7fe2f11a999c0ab2b8ed0c40514bedaa90da9482f6f0888a01eecd"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.0/dub-cli_0.1.0_linux_arm64.tar.gz"
-      sha256 "bd8a7603ba008ecbcbb5215a053d1797ea83fce102b7d6550195088118626f15"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_linux_arm64.tar.gz"
+      sha256 "9ca1ffec89cfa265d2249cf9a31049fe4d59686e5e66a250ba3f4c37b520d66a"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.0/dub-cli_0.1.0_linux_amd64.tar.gz"
-      sha256 "70f4212741f6c735a0490b3a2be9ff347637f30ba21c0854d4251fd214e188c9"
+      url "https://github.com/salmonumbrella/dub-cli/releases/download/v0.1.2/dub-cli_0.1.2_linux_amd64.tar.gz"
+      sha256 "95d1acf5e381135b1b9fac63ca5a0e054a1a7b92fdbcb824564fad53e6a65580"
     end
   end
 
