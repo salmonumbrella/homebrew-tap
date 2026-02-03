7b925fac933c9ba5a2f86ae8fd7dc40618a9e1d6
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2026-02-03 00:20:58 -0800

    fix(notion-cli): use versioned filenames for v0.5.1

--- Formula/notion-cli.rb
+++ Formula/notion-cli.rb
@@ -6,22 +6,22 @@ class NotionCli < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_darwin_arm64.tar.gz"
+      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_0.5.1_darwin_arm64.tar.gz"
       sha256 "3eae83f21d648c6247f96b5b88a2e05c687a836dc6ba43ff4756bbad4bee3fdf"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_darwin_amd64.tar.gz"
+      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_0.5.1_darwin_amd64.tar.gz"
       sha256 "bd6f827fbdecac528742711a120541771f9a19c942d10c5a64651ac0bb2d32d6"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_linux_arm64.tar.gz"
+      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_0.5.1_linux_arm64.tar.gz"
       sha256 "6d67de98a791d947dc9848e86089a9cb41d2a7447e6c1023853f58f1549578e0"
     end
     on_intel do
-      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_linux_amd64.tar.gz"
+      url "https://github.com/salmonumbrella/notion-cli/releases/download/v0.5.1/notion_0.5.1_linux_amd64.tar.gz"
       sha256 "a62f37785458db9c9ee9dc8ad49b20c132ca451e026177bfc86d850f3149d760"
     end
   end
