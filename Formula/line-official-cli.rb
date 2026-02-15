03e8fe07713939f6ff61c1b9f15c2f5c12cd485a
salmonumbrella <182032677+salmonumbrella@users.noreply.github.com>
2026-02-15 11:57:08 -0800

    brew(line-official-cli): build formula from source

--- Formula/line-official-cli.rb
+++ Formula/line-official-cli.rb
@@ -4,36 +4,16 @@
 class LineOfficialCli < Formula
   desc "CLI for LINE Official Account / Messaging API"
   homepage "https://github.com/salmonumbrella/line-official-cli"
-  version "0.4.0"
+  url "https://github.com/salmonumbrella/line-official-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "b4a4cee772ffc856387760846f51921e90cdd5bc4b3c2bd5c2ef42c42c029bf9"
   license "MIT"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/salmonumbrella/line-official-cli/releases/download/v0.4.0/lno_0.4.0_darwin_arm64.tar.gz"
-      sha256 "86c870b88a92b03116fa7f0cbb3b32286825286222d715d056f7495b7ea6201b"
-    end
-    on_intel do
-      url "https://github.com/salmonumbrella/line-official-cli/releases/download/v0.4.0/lno_0.4.0_darwin_amd64.tar.gz"
-      sha256 "d6e26c02c13270b723e03c102352d7380a3b3a8bd5733d5983b6196644fa82fe"
-    end
-  end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/salmonumbrella/line-official-cli/releases/download/v0.4.0/lno_0.4.0_linux_arm64.tar.gz"
-      sha256 "8c2d0d0cd0e070f4208d40c17fd503f2b24ee20ad92208bc3a854c0356d632e7"
-    end
-    on_intel do
-      url "https://github.com/salmonumbrella/line-official-cli/releases/download/v0.4.0/lno_0.4.0_linux_amd64.tar.gz"
-      sha256 "539b30e5a299184fad5755ed12d09839e6eeecf800b855b328e8c1355b86debe"
-    end
-  end
+  depends_on "go" => :build
 
   def install
-    bin.install "lno"
+    system "go", "build", *std_go_args(output: bin/"lno"), "./cmd/lno"
   end
 
   test do
-    system "#{bin}/lno", "--help"
+    assert_match "lno", shell_output("#{bin}/lno version")
   end
 end
